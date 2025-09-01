e72b9dbcab980f63750a056c1b4ca8c1579022ff
Jorge Armando Pacheco Del real <jarmandopacheco@gmail.com>
2025-08-31 23:16:33 -0500

    refactor(characters): improve typing and API response handling
    - Se actualiza el servicio de Characters para usar el tipo ApiResponse en lugar de Domain[] directo.
    - Se ajusta la función getAll para retornar tanto info como results mapeados.
    - Se mejora la tipificación de la respuesta del API y se centraliza la conversión de resultados.
    - Hook useCharacters adaptado para manejar correctamente la estructura de ApiResponse.

--- src/features/characters/hooks/useCharacters.ts
+++ src/features/characters/hooks/useCharacters.ts
@@ -1,35 +1,39 @@
 "use client";
 
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { Domain } from "../types";
 import { charactersService } from "../services";
+import { ApiResponse } from "@/shared/types/pagination.types";
 
 export const useCharacters = (page: number = 1, name?: string) => {
     const [characters, setCharacters] = useState<Domain[]>([]);
+    const [totalPages, setTotalPages] = useState(1);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
 
-    const fetchCharacters = useCallback(async () => {
-        try {
-            setLoading(true);
-            const data = await charactersService.getAll(page, name);
-            setCharacters(data);
-            setError(null);
-        } catch (err) {
-            setError(err instanceof Error ? err.message : "Error desconocido");
-        } finally {
-            setLoading(false);
-        }
-    }, [page, name]);
-
     useEffect(() => {
+        const fetchCharacters = async () => {
+            try {
+                setLoading(true);
+                const data: ApiResponse<Domain> = await charactersService.getAll(page, name);
+
+                setCharacters(data.results);
+                setTotalPages(data.info.pages);
+                setError(null);
+            } catch (err) {
+                setError(err instanceof Error ? err.message : "Error desconocido");
+            } finally {
+                setLoading(false);
+            }
+        };
+
         fetchCharacters();
-    }, [fetchCharacters]);
+    }, [page, name]);
 
     return {
         characters,
+        totalPages,
         loading,
         error,
-        refreshCharacters: fetchCharacters,
     };
 };
