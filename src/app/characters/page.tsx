55953bc76f0ed1d7e030d49bca4145e8e0e700de
Jorge Armando Pacheco Del real <jarmandopacheco@gmail.com>
2025-09-18 08:54:07 -0500

    feat: use Icon component
    Se implementa el uso de los Iconos.

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -6,12 +6,13 @@ import { useCharacters } from "@/features/characters/hooks/useCharacters";
 import Pagination from "@/shared/components/Pagination/Pagination";
 import Input from "@/shared/components/Input/Input";
 import { LinkButton } from "@/shared/components";
+import SearchIcon from "@/shared/components/icons/SearchIcon";
 
 export default function CharactersPage() {
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
   const { characters, totalPages, loading, error } = useCharacters(page, search);
-  const ITEMS_PER_PAGE = Number(process.env.NEXT_PUBLIC_ITEMS_PER_PAGE) || 20;
+  const ITEMS_PER_PAGE = Number(process.env.NEXT_PUBLIC_CHARACTERS_ITEMS_PER_PAGE) || 20;
 
   return (
     <main className="container mx-auto py-8">
@@ -27,36 +28,44 @@ export default function CharactersPage() {
         </LinkButton>
       </div>
 
-      {loading && (
-        <p className="text-blue-500 mt-4">Cargando personajes...</p>
-      )}
-
-      <div className="width-full mb-6">
+      <div className="bg-white w-full my-6 flex items-center gap-2 border border-gray-300 rounded-lg px-3 shadow-sm hover:border-gray-400 focus-within:border-sky-800 focus-within:ring-2 focus-within:ring-sky-400 transition">
+        <SearchIcon className="w-5 h-5 text-gray-500" />
+        <span className="text-gray-300">|</span>
         <Input
-          label="Buscar personaje"
-          placeholder="Escribe un nombre..."
-          fullWidth
+          placeholder="Filtrar Personajes..."
           value={search}
           onChange={(e) => {
             setPage(1);
             setSearch(e.target.value);
           }}
+          className="flex-1 w-full max-w-full focus:outline-none focus:ring-0 focus:border-none shadow-none !ring-0 !outline-none !border-none text-gray-900 placeholder:text-gray-400"
         />
       </div>
 
-      <CharacterList characters={characters} />
+      <div className="relative">
+        {loading && (
+          <div className="flex justify-center items-center mt-4 py-4 w-full h-auto">
+            <div className="flex items-center space-x-2">
+              <div className="animate-spin rounded-full h-4 w-4 border-b-2 border-blue-500"></div>
+              <p className="text-gray-500">Cargando personajes...</p>
+            </div>
+          </div>
+        )}
 
-      {!loading && characters.length === 0 && !error && (
-        <p className="text-gray-500 mt-4">
-          No se encontraron personajes.
-        </p>
-      )}
+        <CharacterList characters={characters} />
 
-      {error && (
-        <p className="text-red-600 font-medium mt-4">
-          {error}
-        </p>
-      )}
+        {!loading && characters.length === 0 && !error && (
+          <p className="text-gray-500 mt-4">
+            No se encontraron personajes.
+          </p>
+        )}
+
+        {error && (
+          <p className="text-red-600 font-medium mt-4">
+            {error}
+          </p>
+        )}
+      </div>
 
       <Pagination
         currentPage={page}
